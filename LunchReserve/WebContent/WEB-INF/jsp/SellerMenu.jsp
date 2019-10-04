3035f7321a5ec6cc9fae15b1bf8ead0504734aed
kodama-yutou <mail.1701134@gmail.com>
2019-10-04 14:16:57 +0900

    管理者メニュー作成

---
+++ LunchReserve/WebContent/WEB-INF/jsp/SellerMenu.jsp
@@ -0,0 +1,39 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>管理者メニュー</title>
+</head>
+<body>
+<table border=1>
+<tr>
+<td>
+<p>予約</p>
+<p><a href="page1">商品別予約一覧</a></p>
+<p><a href="page2">予約履歴</a></p>
+</td>
+<td>
+<p>商品</p>
+<p><a href="page3">商品一覧</a></p>
+<p><a href="page4">商品登録</a></p>
+</td>
+<td>
+<p>売上</p>
+<p><a href="page5">予約商品売上一覧</a></p>
+<p><a href="page6">予約商品分析</a></p>
+</td>
+</tr>
+<tr>
+<td>
+<p>意見</p>
+<p><a href="page7">意見一覧</a></p>
+</td>
+<td>
+<p>スケジュール</p>
+<p><a href="page8">スケジュール</a></p>
+</td>
+</table>
+</body>
+</html>
---
+++ LunchReserve/src/lunchReservation/seller/controller/SellerMenuServlet.java
@@ -0,0 +1,27 @@
+package lunchReservation.seller.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SellerMenuServlet
+ */
+@WebServlet("/SellerMenuServlet")
+public class SellerMenuServlet extends HttpServlet {
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/SellerMenu.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
